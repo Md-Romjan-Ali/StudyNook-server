218f58b14a81abeb25c90e38ef17baa25ff0ac8f
mdromjanali224512-collab <mdromjanali224512@gmail.com>
2026-05-20 12:48:59 +0600

    added my listing and userroom post get and so on

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const cors=require('cors')
 const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config()
-const port= 5000
+const port=process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -29,16 +29,14 @@ async function run() {
 
     const db = client.db("studyNook");
     const roomCollection = db.collection("rooms");
+    const usersRoomsCollection = db.collection("usersrooms");
+    const myListing = db.collection("mylisting");
     
     app.post('/studyrooms',async (req,res)=>{
         const cursor=req.body
         const result= await roomCollection.insertOne(cursor)
         res.send(result)
     })
-//     app.get('/studyrooms',async(req,res)=>{
-//  const result=await roomCollection.find().toArray()
-//         res.send(result)
-//     })
 app.get('/studyrooms', async (req, res) => {
 
     const result = await roomCollection
@@ -55,6 +53,7 @@ app.get('/studyrooms', async (req, res) => {
         const result=await roomCollection.findOne(query)
         res.send(result)
     })
+
     app.delete('/studyrooms/:id',async(req,res)=>{
         const id=req.params.id;
         const query={_id: new ObjectId(id)}
@@ -65,17 +64,46 @@ app.get('/studyrooms', async (req, res) => {
     const {id}=req.params
     console.log(id,'id');
     const updateData=req.body;
-    
   const result=await roomCollection.updateOne(
     {_id:new ObjectId(id)},
   {$set:updateData}
 )
 res.send(result)
   })
-    
+
+      // listing add
+  app.post('/mylistingdata', async (req, res) => {
+    const cursor = req.body;
+  const result = await myListing.insertOne(cursor);
+  res.send(result);
+});
+  // get by email
+  app.get('/mylistingdata/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email };
+  const result = await myListing.find(query).toArray();
+  res.send(result);
+});
+
+    // user add
+    app.post('/usersrooms',async(req,res)=>{
+  const cursor=req.body
+  const result= await usersRoomsCollection.insertOne(cursor)
+  res.send(result)
+    })
+    app.get("/usersrooms",async(req,res)=>{
+  const result=await usersRoomsCollection.find().toArray()
+  res.send(result)
+    })
+    app.get('/usersrooms/:userId',async(req,res)=>{
+  const userId=req.params.userId
+  const query={userId}
+  const result=await usersRoomsCollection.find(query).toArray()
+  res.send(result)
+    })
+
   
-  
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
