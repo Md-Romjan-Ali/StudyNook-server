12cb729fcb29afaee55b18c23c97c2f6fc4cff5c
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-05-21 15:24:59 -0700

    added user update

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express')
 const app = express()
-const cors=require('cors')
-const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -31,78 +31,87 @@ async function run() {
     const roomCollection = db.collection("rooms");
     const usersRoomsCollection = db.collection("usersrooms");
     const myListing = db.collection("mylisting");
-    
-    app.post('/studyrooms',async (req,res)=>{
-        const cursor=req.body
-        const result= await roomCollection.insertOne(cursor)
-        res.send(result)
-    })
-app.get('/studyrooms', async (req, res) => {
 
-    const result = await roomCollection
+    app.post('/studyrooms', async (req, res) => {
+      const cursor = req.body
+      const result = await roomCollection.insertOne(cursor)
+      res.send(result)
+    })
+    app.get('/studyrooms', async (req, res) => {
+
+      const result = await roomCollection
         .find()
         .limit(+req.query.limit || 0)
         .toArray();
 
-    res.send(result);
-});
+      res.send(result);
+    });
 
-    app.get('/studyrooms/:id',async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)}
-        const result=await roomCollection.findOne(query)
-        res.send(result)
+    app.get('/studyrooms/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await roomCollection.findOne(query)
+      res.send(result)
     })
 
-    app.delete('/studyrooms/:id',async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)}
-        const result=await roomCollection.deleteOne(query)
-        res.send(result)
+    app.delete('/studyrooms/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await roomCollection.deleteOne(query)
+      res.send(result)
     })
-  app.patch('/studyrooms/:id',async(req,res)=>{
-    const {id}=req.params
-    console.log(id,'id');
-    const updateData=req.body;
-  const result=await roomCollection.updateOne(
-    {_id:new ObjectId(id)},
-  {$set:updateData}
-)
-res.send(result)
-  })
+    app.patch('/studyrooms/:id', async (req, res) => {
+      const { id } = req.params
+      console.log(id, 'id');
+      const updateData = req.body;
+      const result = await roomCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      )
+      res.send(result)
+    })
 
-      // listing add
-  app.post('/mylistingdata', async (req, res) => {
-    const cursor = req.body;
-  const result = await myListing.insertOne(cursor);
-  res.send(result);
-});
-  // get by email
-  app.get('/mylistingdata/:email', async (req, res) => {
-  const email = req.params.email;
-  const query = { email };
-  const result = await myListing.find(query).toArray();
-  res.send(result);
-});
+    // listing add
+    app.post('/mylistingdata', async (req, res) => {
+      const cursor = req.body;
+      const result = await myListing.insertOne(cursor);
+      res.send(result);
+    });
+    // get by email
+    app.get('/mylistingdata/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await myListing.find(query).toArray();
+      res.send(result);
+    });
 
     // user add
-    app.post('/usersrooms',async(req,res)=>{
-  const cursor=req.body
-  const result= await usersRoomsCollection.insertOne(cursor)
-  res.send(result)
+    app.post('/usersrooms', async (req, res) => {
+      const cursor = req.body
+      const result = await usersRoomsCollection.insertOne(cursor)
+      res.send(result)
     })
-    app.get("/usersrooms",async(req,res)=>{
-  const result=await usersRoomsCollection.find().toArray()
-  res.send(result)
+    app.get("/usersrooms", async (req, res) => {
+      const result = await usersRoomsCollection.find().toArray()
+      res.send(result)
     })
-    app.get('/usersrooms/:userId',async(req,res)=>{
-  const userId=req.params.userId
-  const query={userId}
-  const result=await usersRoomsCollection.find(query).toArray()
-  res.send(result)
+    app.get('/usersrooms/:userId', async (req, res) => {
+      const userId = req.params.userId
+      const query = { userId }
+      const result = await usersRoomsCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.patch('/usersrooms/:id', async (req, res) => {
+      const { id } = req.params
+      console.log(id, 'id');
+      const updateData = req.body;
+      const result = await usersRoomsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      )
+      res.send(result)
     })
 
-  
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
